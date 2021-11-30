06388f4feb0ebbd3b1bb252ef3c9af65a4d5a191
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-11-30 12:32:41 +0700

    config jwt for api gateway

---
+++ src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.example.apigateway.config;
+
+import com.example.apigateway.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes().route("auth", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("lb://auth"))
+                .route("ORDERS-SERVICE", r -> r.path("/orders/**").filters(f -> f.filter(filter)).uri("lb://ORDERS-SERVICE"))
+                .route("SHIPPING-SERVICE", r -> r.path("/shipping/**").filters(f -> f.filter(filter)).uri("lb://SHIPPING-SERVICE")).build();
+    }
+
+}
